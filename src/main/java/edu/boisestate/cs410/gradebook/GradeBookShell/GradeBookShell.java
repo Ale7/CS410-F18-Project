275d4170ec63e21b50f1cb18d862a7f89074036e
Ale7 <alecwooding@gmail.com>
2018-12-08 16:32:21 -0700

    Changed file structure

--- src/main/java/edu/boisestate/cs410/gradebook/GradeBookShell/GradeBookShell.java
+++ src/main/java/edu/boisestate/cs410/gradebook/GradeBookShell/GradeBookShell.java
@@ -1,20 +1,22 @@
+package main.java.edu.boisestate.cs410.gradebook.GradeBookShell;
+
 import com.budhash.cliche.Command;
 import com.budhash.cliche.ShellFactory;
 
 import java.io.IOException;
 import java.sql.*;
 
-public class GradeManager {
+public class GradeBookShell {
     private final Connection db;
 
-    public GradeManager(Connection cxn) {
+    public GradeBookShell(Connection cxn) {
         db = cxn;
     }
 
     public static void main(String[] args) throws IOException, SQLException {
         String dbUrl = args[0];
         try (Connection cxn = DriverManager.getConnection("jdbc:" + dbUrl)) {
-            GradeManager shell = new GradeManager(cxn);
+            GradeBookShell shell = new GradeBookShell(cxn);
             ShellFactory.createConsoleShell("grade-manager", "", shell)
                         .commandLoop();
         }
